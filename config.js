cc18a8169805e13e6682030ef44d7614ee69bf5b
IvanMoch <142510054+IvanMoch@users.noreply.github.com>
2024-10-10 10:41:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const DB_CONFIG = {
 
   connectionLimit: 10,
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PSSWD || 'i26a08m03p',
-  database: process.env.DB_NAME || 'WhatsPirate'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PSSWD,
+  database: process.env.DB_NAME
 
 }
